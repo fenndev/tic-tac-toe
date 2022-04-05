1ad85dd946ebb9cb4bbfe755ac370ede26ce3568
Fenn <fenndev@protonmail.com>
2022-04-05 16:56:13 -0700

    Added basic factory and module functions

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 const gameBoard = (() => {
     let boardArray = {};
+
+    const getBoardNeighbors = (cell) => {
+        return [ getVerticalNeighbors(cell), getHorizontalNeighbors(cell) ]
+    }
+
+    const getHorizontalNeighbors = (cell) => {
+        switch (cell.getCellCol()) {
+            case 0:
+                return {
+                    vertNeighborOne : boardIndex + 1,
+                    vertNeighborTwo : vertNeighborOne + 1
+                }
+            case 1:
+                return {
+                    vertNeighborOne : boardIndex + 1,
+                    vertNeighborTwo : boardIndex - 1
+                }
+            default:
+                return {
+                    vertNeighborOne : boardIndex - 1,
+                    vertNeighborTwo : vertNeighborOne - 1
+                }
+        }
+    }
+
+    const getVerticalNeighbors = (cell) => {
+        let boardIndex = cell.boardIndex;
+        switch (cell.getCellRow()) {
+            case 0:
+                return {
+                    vertNeighborOne : boardIndex + 3,
+                    vertNeighborTwo : vertNeighborOne + 3
+                }
+            case 1:
+                return {
+                    vertNeighborOne : boardIndex + 3,
+                    vertNeighborTwo : boardIndex - 3
+                }
+            default:
+                return {
+                    vertNeighborOne : boardIndex - 3,
+                    vertNeighborTwo : vertNeighborOne - 3
+                }
+        }
+    }
 })();
 
 const displayManager = (() => {
@@ -20,6 +65,14 @@ let Player = (name) => {
 }
 
 const boardCell = (boardIndex) => {
-    let symbol = "";
-    return { boardIndex, symbol };
+
+    const getCellCol = () => {
+        return (boardIndex % 3) + 1;
+    }
+
+    const getCellRow = () => {
+        return (boardIndex % 3) + 1;
+    }
+
+    return { boardIndex, getCellCol, getCellRow }
 } 
