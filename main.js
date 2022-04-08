65c5ae1c629dcd048c56112aefe269134375b594
fenndev <fenndev@protonmail.com>
2022-04-07 19:33:25 -0700

    Added basic display updating functionality

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+// Player
+
+const Player = (name) => {
+    let turnPos;
+    let symbol = "";
+    let wins = 0;
+    return { name, symbol, wins, turnPos };
+};
+
+// Display Manager
+
+const DisplayManager = (() => {
+    let grid = document.querySelector(".grid");
+    const gridCells = grid.querySelectorAll(".grid__cell");
+    console.log(gridCells);
+
+    const updateCellDisplay = (cellToUpdate, currentPlayer) => {
+        cellToUpdate.textContent = currentPlayer.symbol;
+    }
+
+    return { grid, gridCells, updateCellDisplay };
+})();
+
 // Game Manager
 
 const GameManager = (() => {
     const playerOne = Player("Bob");
     const playerTwo = Player("Jeremy");
-    let gameRunning = false;
+    let currentPlayer;
+    let gameRunning = true;
 
     const setTurns = () => {
-        let currentPlayer;
         let randomNum = Math.floor(Math.random() * 2);
+        console.log(randomNum);
         switch(randomNum) {
             case 0:
                 playerOne.symbol = "X";
@@ -21,40 +45,22 @@ const GameManager = (() => {
                 playerTwo.turnPos = 0;
         }
         currentPlayer = (playerOne.turnPos == 0 ? playerOne : playerTwo);
-        return currentPlayer;
+        console.log(currentPlayer);
     }
     
-    for (cell in DisplayManager.gridCells) {
+    DisplayManager.gridCells.forEach(cell => {
         cell.addEventListener('click', () => {
             markCell(cell, currentPlayer);
         });
-    };
+    });
 
     const markCell = (cell, currentPlayer) => {
         if(gameRunning) {
-            updateCellDisplay(cell, currentPlayer);
+            DisplayManager.updateCellDisplay(cell, currentPlayer);
         }   
     }
+
+    return { playerOne, playerTwo, markCell, setTurns };
 })();
 
-// Display Manager
-
-const DisplayManager = (() => {
-    let grid = document.querySelector(".grid");
-    const gridCells = grid.querySelectorAll(".grid__cell");
-
-    const updateCellDisplay = (cellToUpdate, currentPlayer) => {
-        cellToUpdate.textContent = currentPlayer.symbol;
-    }
-
-    return { grid, gridCells, updateCellDisplay };
-})();
-
-// Player
-
-const Player = (name) => {
-    let turnPos;
-    let symbol = "";
-    let wins = 0;
-    return { name, symbol, wins, turnPos };
-};
+GameManager.setTurns();
