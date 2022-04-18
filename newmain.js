b704e2aac958b47b6e2e62dc2bff72ff1193b49a
fenndev <fenndev@protonmail.com>
2022-04-17 23:53:21 -0700

    Added & revised GameManger to newmain.js

--- newmain.js
+++ newmain.js
@@ -22,3 +22,54 @@ const DOMManager = (() => {
 
     return { getGridCells, updateCellDisplay };
 })();
+
+// Game Manager
+
+const GameManager = (() => {
+    const playerOne = Player("Bob");
+    const playerTwo = Player("Jeremy");
+    let currentPlayer;
+    let gameRunning = true;
+
+    const playGame = () => {
+        setTurns();
+        makeCellsClickable();
+    }
+
+    const setTurns = () => {
+        let randomNum = Math.floor(Math.random() * 2);
+        switch(randomNum) {
+            case 0:
+                playerOne.symbol = "X";
+                playerTwo.symbol = "O";
+            default:
+                playerOne.symbol = "O";
+                playerTwo.symbol = "X";
+        }
+        currentPlayer = (randomNum == 0 ? playerOne : playerTwo);
+    }
+
+    const turnShift = () => {
+        if(currentPlayer == playerOne)
+            currentPlayer = playerTwo;
+        else
+            currentPlayer = playerOne;
+    }
+
+    const makeCellsClickable = () => {
+        DisplayManager.getGridCells().forEach(cell => {
+            cell.addEventListener('click', () => {
+                markCell(cell, currentPlayer);
+            });
+        });
+    }
+
+    const markCell = (cell, currentPlayer) => {
+        if(gameRunning && cell.textContent == "*") {
+            DisplayManager.updateCellDisplay(cell, currentPlayer);
+            currentPlayer.currentTurn++;
+        }   
+    }
+    
+    return { playGame };
+})();
