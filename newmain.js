7122a97e24209f537139c78fc6f365a940be89cf
fenndev <fenndev@protonmail.com>
2022-04-17 23:51:13 -0700

    Added DOMManager constructor to newmain.js

--- newmain.js
+++ newmain.js
@@ -5,3 +5,20 @@ const Player = (name) => {
     let wins = 0;
     return { name, symbol, wins };
 };
+
+// DOM Manager
+
+const DOMManager = (() => {
+    let grid = document.querySelector(".grid");
+    const gridCells = grid.querySelectorAll(".grid__cell");
+
+    const updateCellDisplay = (cellToUpdate, currentPlayer) => {
+        cellToUpdate.textContent = currentPlayer.symbol;
+    }
+
+    const getGridCells = () => {
+        return gridCells;
+    }
+
+    return { getGridCells, updateCellDisplay };
+})();
