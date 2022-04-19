314e001fa9360fc78d18eeb8a64dff6958a591f1
fenndev <fenndev@protonmail.com>
2022-04-19 16:32:15 -0700

    Show modal on win & fixed win bug

--- main.js
+++ main.js
@@ -11,11 +11,13 @@ const Player = (name) => {
 const DOMManager = (() => {
     const startButton = document.querySelector(".start-btn");
     const modalSubmitButton = document.querySelector(".submit-btn");
-    const modalForm = document.querySelector(".modal__prompt");
-    const modal = document.querySelector(".modal");
+    const modalForm = document.querySelector("#form");
+    const modal = document.querySelector("#form-display");
     const currentPlayerDisplay = document.querySelector(".current-player");
     const playerOneNameInput = document.querySelector("#player-one");
     const playerTwoNameInput = document.querySelector("#player-two");
+    const winName = document.querySelector("#win-name");
+    const winModal = document.querySelector("#win-display");
     let grid = document.querySelector(".grid");
     const gridCells = grid.querySelectorAll(".grid__cell");
     const updateCellDisplay = (cellToUpdate, currentPlayer) => cellToUpdate.textContent = currentPlayer.symbol;
@@ -23,7 +25,6 @@ const DOMManager = (() => {
     const getStartButton = () => startButton;
     const getSubmitButton = () => modalSubmitButton;
     const getModalForm = () => modalForm;
-    const getCurrentPlayerDisplay = () => currentPlayerDisplay;
     const showModalForm = () => {
         startButton.style.display = "none";
         modalForm.style.display = "flex";
@@ -33,6 +34,11 @@ const DOMManager = (() => {
         modal.style.display = "none";
     }
 
+    const showWinModal = (currentPlayer) => {
+        winName.textContent = `${currentPlayer.name} won!`;
+        winModal.classList.remove("hidden");
+    }
+
     const updateCurrentPlayerDisplay = (currentPlayer) => currentPlayerDisplay.textContent = `${currentPlayer.name}'s Turn!`;
 
     const getPlayerOne = () => playerOneNameInput.value;
@@ -40,7 +46,7 @@ const DOMManager = (() => {
 
 
 
-    return { getGridCells, updateCellDisplay, getStartButton, getSubmitButton, showModalForm, hideModal, getModalForm, updateCurrentPlayerDisplay, getPlayerOne, getPlayerTwo };
+    return { getGridCells, updateCellDisplay, getStartButton, getSubmitButton, showModalForm, hideModal, getModalForm, updateCurrentPlayerDisplay, getPlayerOne, getPlayerTwo, showWinModal };
 })();
 
 // Game Manager
@@ -76,8 +82,11 @@ const GameManager = (() => {
     const markCell = (cell, currentPlayer) => {
         if(gameRunning && cell.textContent == "*") {
             DOMManager.updateCellDisplay(cell, currentPlayer);
-            if(checkForWin())
-                console.log(`Congratulations, ${currentPlayer.name}! You win!`);
+            if(checkForWin()) {
+                DOMManager.showWinModal(currentPlayer);
+            }
+            else if(checkForTie())
+                DOMManager.showTieModal();
             else turnShift();
         }
     }
@@ -108,6 +117,7 @@ const GameManager = (() => {
         let tempResult = false;
         for (let section in sectionArray) {
             if(checkGridSection(sectionArray[section])) {
+                console.log(sectionArray[section]);
                 tempResult = true;
                 break;
             }
@@ -115,14 +125,21 @@ const GameManager = (() => {
         return tempResult;
     }
 
+    const checkForTie = () => {
+        let gridArray = Array.from(DOMManager.getGridCells());
+        if(gridArray.every(cell => isNotEmptyCell(cell.textContent))) return true;
+    }
+
     const getGridSection = (grid, sectionType, sectionNum) => {
         if(sectionType != 'diag') {return grid.filter(cell => cell.getAttribute(`data-${sectionType}`) == sectionNum)}
-        else {return grid.filter(cell => cell.getAttribute(`data-${sectionType}`) == sectionNum) || cell.getAttribute(`data-${sectionType}`) == 'center'};
+        else {return grid.filter(cell => (cell.getAttribute(`data-${sectionType}`) == sectionNum) || (cell.getAttribute(`data-${sectionType}`) == 'center'))};
     }
 
     const checkGridSection = (sectionToCheck) => { if(sectionToCheck.every(cell => doesSymbolMatch(cell.textContent))) return true; }
     
     const doesSymbolMatch = (symbol) => { if(symbol == currentPlayer.symbol) return true; }
+
+    const isNotEmptyCell = (cellSymbol) => { if(cellSymbol != "*") return true }
 
     return { playGame };
 })();
